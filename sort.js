9b44aa6603871c0f44831e368122277620a9edea
sakhawat777 <biplobbms@gmail.com>
2021-11-09 14:04:55 +0600

    array sort file updated

--- sort.js
+++ sort.js
@@ -7,6 +7,14 @@ const alphabet = ['d', 'a', 'c', 'b'];
 const descend = array.sort((a, b) => (a > b ? -1 : 1));
 console.log(descend); // [ 6, 5, 4, 3, 2, 1 ]
 
+// sort in descending order
+const descend2 = array.sort((a, b) => b - a);
+console.log(descend2); // [ 6, 5, 4, 3, 2, 1 ]
+
 // sort in ascending order
 const ascend = alphabet.sort((a, b) => (a > b ? 1 : -1));
 console.log(ascend); // [ 'a', 'b', 'c', 'd' ]
+
+// default in ascending order
+const alpha = alphabet.sort();
+console.log(alpha); // [ 'a', 'b', 'c', 'd' ]
