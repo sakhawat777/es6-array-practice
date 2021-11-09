4e9aca20ce421713b4d39eb4c404747830172c7e
sakhawat777 <biplobbms@gmail.com>
2021-11-09 15:50:02 +0600

    includes in array practice done

---
+++ includes.js
@@ -0,0 +1,5 @@
+// includes() This method checks if an array includes the element that passes the condition, returning true or false as appropriate.
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(array.includes(7)); // true
+console.log(array.includes(10)); // false
