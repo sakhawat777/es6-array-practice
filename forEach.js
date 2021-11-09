aec75c375c38828e1031b10bfa57cf0aa05d60cd
sakhawat777 <biplobbms@gmail.com>
2021-11-09 15:04:53 +0600

    forEach in array practice done

---
+++ forEach.js
@@ -0,0 +1,8 @@
+// forEach() This method helps to loop over array by executing a provided callback function for each element in an array.
+
+const array = [1, 2, 3, 4];
+array.forEach((element) => console.log(element));
+//1
+//2
+//3
+//4
