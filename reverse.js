b67eabbecc549ea6bcf12f932241bad684059bcc
sakhawat777 <biplobbms@gmail.com>
2021-11-09 13:23:46 +0600

    Array reverse Practice Done

---
+++ reverse.js
@@ -0,0 +1,11 @@
+// reverse() This method reverses an array in place. Element at last index will be first and element at 0 index will be last.
+
+const numbers = [20, 25, 30, 32, 40];
+const num2 = numbers.reverse();
+console.log(numbers); // [ 40, 32, 30, 25, 20 ]
+console.log(num2); // [ 40, 32, 30, 25, 20 ]
+
+const array = [1, 2, 3, 4, 5, 6];
+const reversed = array.reverse();
+console.log(array); // [ 6, 5, 4, 3, 2, 1 ]
+console.log(reversed); // [ 6, 5, 4, 3, 2, 1 ]
