3a892ade00681e222263655c53730efe37a164b4
sakhawat777 <biplobbms@gmail.com>
2021-11-09 15:56:46 +0600

    findIndex in array practice done

---
+++ findIndex.js
@@ -0,0 +1,5 @@
+// findIndex() This method returns the index of the first element in an array that pass the test in a testing function.
+
+const array = ['Biplob', 'Sakhawat', 'Tina', 'Bina'];
+const indexFinder = array.findIndex((element) => element === 'Sakhawat');
+console.log(indexFinder); // 1
