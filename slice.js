8823bc76a804139af8a26ef0f94446ff6ee5dd01
sakhawat777 <biplobbms@gmail.com>
2021-11-09 12:02:20 +0600

    Array slice Practice Done

--- slice.js
+++ slice.js
@@ -1,1 +1,11 @@
 // slice( ) This method returns a new array with specified start to end elements.
+
+const numbers = [20, 25, 30, 32, 40];
+const num2 = numbers.slice(1, 3);
+console.log(numbers); // [ 20, 25, 30, 32, 40 ]
+console.log(num2); // [ 25, 30 ]
+
+const array = [1, 2, 3, 4, 5, 6];
+const sliced = array.slice(0, 4);
+console.log(array); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(sliced); // [ 1, 2, 3, 4 ]
