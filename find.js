c8d02aadb7bb274f21e805156f6df126bea30dc3
sakhawat777 <biplobbms@gmail.com>
2021-11-09 11:50:47 +0600

    Array find Practice Done

---
+++ find.js
@@ -0,0 +1,11 @@
+// This method returns the value of the first element in an array that pass the test in a testing function.
+
+const numbers = [20, 25, 30, 32, 40];
+const num2 = numbers.find((num) => num > 20);
+console.log(numbers); // [ 20, 25, 30, 32, 40 ]
+console.log(num2); // 25
+
+const array = [1, 2, 3, 4, 5, 6];
+const found = array.find((element) => element === 3 || element === 5);
+console.log(array); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(found); // 3
