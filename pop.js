e788be9b83a5d18d463b15f5c5cb16568791c284
sakhawat777 <biplobbms@gmail.com>
2021-11-09 13:17:48 +0600

    Array pop Practice Done

---
+++ pop.js
@@ -0,0 +1,11 @@
+// pop() This method removes the last element from the end of array and returns that element.
+
+const numbers = [20, 25, 30, 32, 40];
+const num2 = numbers.pop();
+console.log(numbers); // [ 20, 25, 30, 32 ]
+console.log(num2); // 40
+
+const array = [1, 2, 3, 4, 5, 6];
+const popped = array.pop();
+console.log(array); // [ 1, 2, 3, 4, 5 ]
+console.log(popped); // 6
