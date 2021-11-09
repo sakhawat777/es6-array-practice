9f223cf4d8ce5a41e9305eb55022b61e38a7ed84
sakhawat777 <biplobbms@gmail.com>
2021-11-09 16:14:29 +0600

    unshift in array practice done

---
+++ unshift.js
@@ -0,0 +1,6 @@
+// unshift() This method adds one or more elements to the beginning of an array and returns the new length of the array.
+
+const array = ['Biplob', 'Sakhawat', 'Tina', 'Bina'];
+console.log(array.unshift('Mina', 'Raja')); // 6
+console.log(array);
+// [ 'Mina', 'Raja', 'Biplob', 'Sakhawat', 'Tina', 'Bina' ]
