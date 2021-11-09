cae5d24c6331532f479631d1fbc755da6defa51c
sakhawat777 <biplobbms@gmail.com>
2021-11-09 16:01:14 +0600

    indexOf in array practice done

---
+++ indexOf.js
@@ -0,0 +1,5 @@
+// indexOf() This method returns the index of the first occurrence of the specified element in the array, or -1 if it is not found.
+
+const array = ['Biplob', 'Sakhawat', 'Tina', 'Bina'];
+const indexFinder = array.indexOf('Tina');
+console.log(indexFinder); // 2
