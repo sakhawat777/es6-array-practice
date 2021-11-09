4685800c24d1c01fc2b4db5db8ed27f8e2728a76
sakhawat777 <biplobbms@gmail.com>
2021-11-09 15:13:41 +0600

    concat in array practice done

---
+++ concat.js
@@ -0,0 +1,7 @@
+// concat() This method is used to merge two or more arrays and returns a new array, without changing the existing arrays.
+
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+console.log(array1.concat(array2)); // [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+console.log(array1); // [ 'a', 'b', 'c' ]
+console.log(array2); // [ 'd', 'e', 'f' ]
