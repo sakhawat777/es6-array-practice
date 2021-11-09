759f87c647fe40e1f29718a6485691f7f29f16c7
sakhawat777 <biplobbms@gmail.com>
2021-11-09 13:43:03 +0600

    Array fill Practice Done

---
+++ fill.js
@@ -0,0 +1,5 @@
+// fill() This method fills the elements in an array with a static value and returns the modified array.
+
+const array = new Array(4);
+console.log(array); // [ empty, empty, empty, empty ]
+console.log(array.fill(77)); // [ 77, 77, 77, 77 ]
