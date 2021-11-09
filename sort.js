1aa8bc5fa47466963902ababda27b6fe9fecc382
sakhawat777 <biplobbms@gmail.com>
2021-11-09 13:54:33 +0600

    Array sort Practice Done

---
+++ sort.js
@@ -0,0 +1,12 @@
+// sort() This method is used to arrange/sort array’s elements either in ascending or descending order.
+
+const array = [1, 2, 3, 4, 5, 6];
+const alphabet = ['d', 'a', 'c', 'b'];
+
+// sort in descending order
+const descend = array.sort((a, b) => (a > b ? -1 : 1));
+console.log(descend); // [ 6, 5, 4, 3, 2, 1 ]
+
+// sort in ascending order
+const ascend = alphabet.sort((a, b) => (a > b ? 1 : -1));
+console.log(ascend); // [ 'a', 'b', 'c', 'd' ]
