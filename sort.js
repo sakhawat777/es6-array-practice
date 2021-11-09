65790c54e120879b6e884ffd36a845fdf1c3102e
sakhawat777 <biplobbms@gmail.com>
2021-11-09 14:09:20 +0600

    array sort file updated

--- sort.js
+++ sort.js
@@ -12,6 +12,14 @@ const descend2 = array.sort((a, b) => b - a);
 console.log(descend2); // [ 6, 5, 4, 3, 2, 1 ]
 
 // sort in ascending order
+const ascend2 = array.sort((a, b) => a - b);
+console.log(ascend2); // [1, 2, 3, 4, 5, 6]
+
+// default in ascending order
+const ascend3 = array.sort();
+console.log(ascend3); // [1, 2, 3, 4, 5, 6]
+
+// sort in ascending order
 const ascend = alphabet.sort((a, b) => (a > b ? 1 : -1));
 console.log(ascend); // [ 'a', 'b', 'c', 'd' ]
 
