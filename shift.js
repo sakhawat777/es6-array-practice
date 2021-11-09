37e0a53c98fb2c539d2df7448d161d4d3c93b2cd
sakhawat777 <biplobbms@gmail.com>
2021-11-09 16:05:57 +0600

    shift in array practice done

---
+++ shift.js
@@ -0,0 +1,5 @@
+// shift() This method removes the first element from an array and returns that element.
+
+const array = ['Biplob', 'Sakhawat', 'Tina', 'Bina'];
+console.log(array.shift()); // Biplob
+console.log(array); // [ 'Sakhawat', 'Tina', 'Bina' ]
